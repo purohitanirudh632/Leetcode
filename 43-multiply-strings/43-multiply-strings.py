4962be0f7f9e5140b0ad00e53370eb23ed58cd42
purohitanirudh632 <99579437+purohitanirudh632@users.noreply.github.com>
2022-08-27 22:43:59 +0530

    Time: 76 ms (55.46%), Space: 13.9 MB (31.15%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        conv1 = int(num1)
+        conv2 = int(num2)
+        
+        mul = conv1 * conv2;
+        
+        strings = str(mul)
+        
+        return strings
+        
